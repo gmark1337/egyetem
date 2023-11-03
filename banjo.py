ceaef332c6e3537ef89891850513f1d8bea43a87
gmark1337 <149498556+gmark1337@users.noreply.github.com>
2023-11-03 14:28:33 +0100

    Delete banjo.py

--- banjo.py
+++
@@ -1,9 +0,0 @@
-def are_you_playing_banjo(name):
-    if name[0].lower() == "r":
-        return name + ' is playing Banjo'
-    else:
-        return name + ' is not playing Banjo'
-
-
-nev = input("Name!: ")
-print(are_you_playing_banjo(nev))
